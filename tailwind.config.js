e726b9c9bbf8d6fc8af551dbdb4caa137417809b
Christopher Riding <chris@superduper.co.nz>
2021-10-26 22:29:56 +1300

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 // tailwind.config.js
 module.exports = {
-    mode: 'jit',
     purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
     corePlugins: {
        fontFamily: false,
